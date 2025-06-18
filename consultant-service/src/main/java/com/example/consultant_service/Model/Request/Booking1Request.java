f0bfac778493e2b615ed3ba8cb2d2671fd4b87df
CongNghia1 <nguyenlenghiawork@gmail.com>
2025-06-18 21:33:59 +0700

    update(booking): request

---
+++ consultant-service/src/main/java/com/example/consultant_service/Model/Request/Booking1Request.java
@@ -0,0 +1,21 @@
+package com.example.consultant_service.Model.Request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+
+public class Booking1Request {
+
+    private String staff_uuid;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yy")
+    private LocalDateTime availableDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yy HH:mm")
+    private LocalDateTime startTime;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yy HH:mm")
+    private LocalDateTime endTime;
+}
